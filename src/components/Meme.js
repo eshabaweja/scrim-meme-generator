100380e3d60476a5d176ba216a066cba0440dfaa
eshabaweja <bawejaesha02@gmail.com>
2022-02-20 21:49:53 +0530

    no api for now

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,20 +1,26 @@
 import React from "react";
+import memesData from "../memesData"
+export default function Meme() {
 
-function Meme() {
+  const [memeImage, setMemeImage] = React.useState("")
 
-  function getMemeImage(){
-    console.log(`veronica, open the door`)
-    return
+
+
+  function getMemeImage() {
+   const memesArray = memesData.data.memes
+    const randomNumber = Math.floor(Math.random() * memesArray.length)
+    setMemeImage(memesArray[randomNumber].url)
   }
+
+
   return (
     <div className="Meme">
       <div className="meme--form">
         <input className="meme--text" type="text" placeholder="One does not simply" />
         <input className="meme--text" type="text" placeholder="Walk into Mordor" />
-        <button className="meme--button" onClick={getMemeImage}> Get a different meme image </button>
+        <button className="meme--button" onClick={getMemeImage}> Get a brand-frickin'-new meme image </button>
+        <img src={memeImage} className="meme--image" />
       </div>
     </div>
   );
 }
-
-export default Meme;
