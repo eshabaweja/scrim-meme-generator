dc87670887ab0be33ec244194cc4e8abd20d26b7
eshabaweja <bawejaesha02@gmail.com>
2022-03-02 23:21:15 +0530

    meme

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,14 +2,23 @@ import React from "react";
 import memesData from "../memesData"
 export default function Meme() {
 
-  const [memeImage, setMemeImage] = React.useState("")
+/*   const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg") */
+const [meme, setMeme] = React.useState({
+                              topText: "",
+                              bottomText: "", 
+                              randomImage: "http://i.imgflip.com/1bij.jpg"
+                            })
 
-
+const [allMemeImages,setAllMemeImages] = React.useState(memesData)
 
   function getMemeImage() {
-   const memesArray = memesData.data.memes
+   const memesArray = allMemeImages.data.memes
     const randomNumber = Math.floor(Math.random() * memesArray.length)
-    setMemeImage(memesArray[randomNumber].url)
+    const memeUrl = memesArray[randomNumber].url
+    setMeme(prevMeme =>({
+      ...prevMeme,
+      randomImage: memeUrl
+    }))
   }
 
 
@@ -19,7 +28,7 @@ export default function Meme() {
         <input className="meme--text" type="text" placeholder="One does not simply" />
         <input className="meme--text" type="text" placeholder="Walk into Mordor" />
         <button className="meme--button" onClick={getMemeImage}> Get a brand-frickin'-new meme image </button>
-        <img src={memeImage} className="meme--image" />
+        <img src={meme.randomImage} className="meme--image" />
       </div>
     </div>
   );
